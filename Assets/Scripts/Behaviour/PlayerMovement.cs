e1f2408030ee980cdc1777f8289296f0a5133ed7
ekaeshic <rifkialfiansyah99@gmail.com>
2022-05-12 17:45:35 +0700

    Character Movement done + other
    What's Added :
    
    PlayerMovement Script
    - Character can dash (for now using button LeftShift)
    - Buffer Double Jump feature so that the Character can buffer the jump while the Character is dashing
    - Changing isGrounded() Method name to checkGround() and call it once in isGrounded variable.
    
    Scene :
    - Added 2D Physics Material called NoFrictionMaterial with 0 Friction and 0 Bounciness to the player(And any other mob in the future).
    - Deleting BillingMode.json (idk why this is added)

--- Assets/Scripts/Behaviour/PlayerMovement.cs
+++ Assets/Scripts/Behaviour/PlayerMovement.cs
@@ -24,15 +24,27 @@ public class PlayerMovement : MonoBehaviour
     [Tooltip("Set value from 0 to 1.")]
     [SerializeField] float airHorizontalDrag = 0.2f;
 
+    [Header("Dash")]
+    [SerializeField] float dashCooldown = 2f;
+    [Tooltip("The player could stay in Anti gravity for set amount of time")]
+    [SerializeField] float dashDuration = 1f;
+    [SerializeField] float dashForce = 10f;
+
     float coyoteTime;
     float jumpEndEarlyTime;
     float jumpBufferTime;
+    float dashCooldownTimer;
+    float dashTimer;
 
     bool jumping;
     bool jumpButtonReleased;
     bool endedJumpEarly;
+    bool doubleJump;
+    bool isGrounded;
+    bool doubleJumpIsValid;
 
     float horizontal;
+    float direction; //Track the current direction the player headed
 
     private void Awake()
     {
@@ -41,22 +53,49 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        isGrounded = checkGrounded();
         //Get Input as float
         horizontal = Input.GetAxisRaw("Horizontal");
 
+        //Tracking last face direction before dash
+        if(horizontal > 0 && dashTimer < 0f) //Player is facing right
+        {
+            direction = 1f;
+        }
+        else if(horizontal < 0 && dashTimer <0f) //Player is facing left
+        {
+            direction = -1f;
+        }
+
         //Time dependant variables
-        if (isGrounded()) coyoteTime = coyoteJumpTime; //Coyote time will remain true so the player could jump anytime the player touches the ground.
+        if (isGrounded)
+        {
+            doubleJumpIsValid = true;
+            coyoteTime = coyoteJumpTime; //Coyote time will remain true so the player could jump anytime the player touches the ground.
+        } 
         else coyoteTime -= Time.deltaTime; //Coyote time will count a few seconds after ungrounded
         jumpBufferTime -= Time.deltaTime;
         jumpEndEarlyTime -= Time.deltaTime;
+        dashTimer -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime;
 
         if (Input.GetKeyDown(KeyCode.Z))
         {
             jumpBufferTime = jumpBuffer;
             jumpButtonReleased = false;
-            if (coyoteTime > 0f && isGrounded())
+            if (coyoteTime > 0f && isGrounded)
             {
                 jumping = true;
+            }
+            if(doubleJumpIsValid && !isGrounded && dashTimer <0f)
+            {
+                doubleJump = true;
+                doubleJumpIsValid = false;
+            }
+            //Buffer jump while dashing
+            else if(dashTimer > 0f && doubleJumpIsValid)
+            {
+                hasBufferedDoubleJump = true;
             }
         }
 
@@ -64,13 +103,19 @@ public class PlayerMovement : MonoBehaviour
         {
             jumpButtonReleased = true;
         }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashCooldownTimer <= 0)
+        {
+            dashCooldownTimer = dashCooldown;
+            dashTimer = dashDuration;
+        }
 
         //Jump Ended Early
         if (jumpButtonReleased && jumpEndEarlyTime <= 0f && rb.velocity.y > 0) endedJumpEarly = true;
         else endedJumpEarly = false;
     }
 
-    bool hasBufferedJump => isGrounded() && jumpBufferTime > 0f;
+    bool hasBufferedJump => isGrounded && jumpBufferTime > 0f;
+    bool hasBufferedDoubleJump;
 
     private void FixedUpdate()
     {
@@ -79,20 +124,35 @@ public class PlayerMovement : MonoBehaviour
         float speedDiff = targetSpeed - rb.velocity.x;
         float accelRate;
         //Apply air drag while midAir, false otherwise
-        if (!isGrounded()) accelRate = Mathf.Abs(targetSpeed) > 0.01f ? acceleration : decceleration * airHorizontalDrag; 
+        if (!isGrounded) accelRate = Mathf.Abs(targetSpeed) > 0.01f ? acceleration : decceleration * airHorizontalDrag; 
         else accelRate = Mathf.Abs(targetSpeed) > 0.01f ? acceleration : decceleration;
         float movement = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, velPower) * Mathf.Sign(speedDiff);
         rb.AddForce(movement * Vector2.right);
 
         //Jump if onKeyDown or has buffered jump(pressing jump while midAir)
-        if(jumping || hasBufferedJump)
+        if(jumping || hasBufferedJump || doubleJump)
         {
             jumpBufferTime = 0f;
             jumping = false;
+            doubleJump=false;
+            Jump();
+        }
+        else if (hasBufferedDoubleJump && dashTimer < 0f)
+        {
+            doubleJump = false;
+            hasBufferedDoubleJump = false;
             Jump();
         }
         //Drag the player down if the jump button is released
-        if (endedJumpEarly) rb.velocity = !isGrounded() && rb.velocity.y > 0 ? new Vector2(rb.velocity.x, rb.velocity.y * jumpEndEarlyModifier) : rb.velocity;
+        if (endedJumpEarly) rb.velocity = !isGrounded && rb.velocity.y > 0 ? new Vector2(rb.velocity.x, rb.velocity.y * jumpEndEarlyModifier) : rb.velocity;
+
+        //Dash
+        if (dashTimer > 0f)
+        {
+            //Make the player in anti gravity state for a short period of time while dashing
+            rb.velocity = new Vector2(direction * dashForce, 0f); //Use this method if u guys prefer constant speed while dashing rather than using Force
+            //rb.AddForce(new Vector2(direction * dashForce, 0), ForceMode2D.Impulse);
+        }
     }
 
     void Jump()
@@ -102,7 +162,7 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(Vector2.up* jumpForce, ForceMode2D.Impulse);
     }
 
-    bool isGrounded()
+    bool checkGrounded()
     {
         int groundMask = 1 << groundLayer;
         return Physics2D.Raycast(rb.position, Vector2.down, transform.localScale.y + 0.01f, groundMask);
