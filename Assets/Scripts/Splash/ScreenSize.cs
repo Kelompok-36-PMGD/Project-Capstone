1486a8a167a612b21852cfcdf2565717ef97d979
parkerkrane <parkerkrane123@gmail.com>
2022-05-12 21:30:06 +0700

    firstcommit

---
+++ Assets/Scripts/Splash/ScreenSize.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ScreenSize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    List<int> widths = new List<int>() { 1280, 1366, 1920 };
+    List<int> heights = new List<int>() { 720, 768, 1080 };
+
+    public void SetScreenSize (int index)
+    {
+        bool fullscreen = Screen.fullScreen;
+        int width = widths[index];
+        int height = heights[index];
+        Screen.SetResolution(width, height, fullscreen);
+    }
+
+    public void SetFullscreen(bool _fullscreen)
+    {
+        Screen.fullScreen = _fullscreen;
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Project-Capstone
+
+Unity Version Used : 2020.33.3f1
+
+Untuk pertama kali pull, terdapat beberapa hal yang harus dilakukan.
+1. Pertama-tama buatlah Project baru di Unity melalui Unity Hub. (Direkomendasikan menggunakan versi yang sama)
+2. Download file project pada github ini dan taruh di folder project anda.
+3. (TBD) Mungkin terdapat beberapa packages yang perlu diinstal, Anda harus menginstalnya pada Project anda sendiri.
+
+Untuk melakukan push progress, Anda harus menjadi tim dari project ini.
+Cara melakukan push pertama kali, harap mengikuti tutorial : https://gist.github.com/mindplace/b4b094157d7a3be6afd2c96370d39fad
+
+Current used Package :
+- Cinemachine v2.6.11
