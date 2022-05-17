7eb735666ec68dd5bddf7235d1069510d4607207
ekaeshic <rifkialfiansyah99@gmail.com>
2022-05-17 16:21:41 +0700

    Improve ManaSystem and Skill Scripts

--- Assets/Scripts/Behaviour/Player/Skill/Mana.cs
+++ Assets/Scripts/Behaviour/Player/Skill/Mana.cs
@@ -1,38 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Mana : MonoBehaviour
 {
+    [Header("Objects")]
     public Health mana;
+    public Slider slider;
+    public Slider movingSlider;
+    public float _sliderSpeed = 5f;
+    public ParticleSystem _particleSys;
     [Header("Regenerate")]
-    public bool RegenOverTime = true;
-    public int regenValue = 1;
-    [Range(0,20)]public float regenRate = 2f;
+    public bool _RegenOverTime = true;
+    public int _regenValue = 1;
+    [Range(0,20)]public float _regenRate = 2f;
 
     float rate=0f;
+    ParticleSystem.EmissionModule emission;
+    private void Awake()
+    {
+        emission = _particleSys.emission;
+        slider.maxValue = mana.maxValue;
+        movingSlider.maxValue = mana.maxValue;
+        slider.value = mana.value;
+        movingSlider.value = slider.value;
+    }
 
     private void Update()
     {
-        if (RegenOverTime)
+        slider.value = mana.value;
+        //Move the slider overtime until it reach the same as the mana value
+        if (slider.value < movingSlider.value)
+        {
+
+            emission.rateOverTime = 10;
+            movingSlider.value -= Time.deltaTime * _sliderSpeed;
+            //movingSlider.value = movingSlider.value - _sliderSpeed < slider.value ? movingSlider.value = mana.value : movingSlider.value - mana.value;
+        }
+        else
+        {
+            movingSlider.value = mana.value;
+            emission.rateOverTime = 0;
+        }
+
+
+        if (_RegenOverTime)
         {
             //Mana calculation is : Mana per seconds = RegenRate * regenValue. (ex: regenRate 5 and regenValue 2 is equal to 10 mana per seconds
-            rate += Time.deltaTime * regenRate;
+            rate += Time.deltaTime * _regenRate;
             if(rate > 1f)
             {
-                mana.value = mana.value + regenValue > mana.maxValue ? mana.maxValue : mana.value + regenValue;
+                mana.value = mana.value + _regenValue > mana.maxValue ? mana.maxValue : mana.value + _regenValue;
                 rate = 0;
             }
         }
     }
 
-    void GainMana(int value)
+    public void GainMana(int value)
     {
         mana.value = mana.value + value > mana.maxValue ? mana.maxValue : mana.value + value;
     }
 
-    void Decrease(int value)
+    public void Decrease(int value)
     {
-        mana.value = mana.value - value < 0 ? 0 : mana.value - value;
+        if(mana.value - value > 0)
+        {
+            mana.value -= value;
+        }
+        else
+        {
+            //show not enough mana;
+            Debug.Log("Not enough mana");
+        }
     }
 }
--- Assets/Scripts/Behaviour/Player/Skill/Skills.cs
+++ Assets/Scripts/Behaviour/Player/Skill/Skills.cs
@@ -7,7 +7,26 @@ public enum AttackType
     //Change skills name here
     NORMAL, HARIMAU, KHODAM2, KHODAM3
 }
+
+[RequireComponent(typeof(Mana))]
 public class Skills : MonoBehaviour
 {
-    
+    private Mana mana;
+    [Header("Skills")]
+    public SkillList _currentKhodam;
+    public List<SkillList> _khodamList;
+
+    private void Awake()
+    {
+        mana = GetComponent<Mana>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            //Usekhodam & damage enemy if on range
+            mana.Decrease(_currentKhodam.manaCost);
+        }
+    }
 }
